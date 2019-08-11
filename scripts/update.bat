8106dd48a8722bacaea28e3952387c9de18b612e
Kevin van Rijn <6368561+kevinvanrijn@users.noreply.github.com>
2019-08-11 20:16:34 +0200

    Update git config

--- scripts/update.bat
+++ scripts/update.bat
@@ -15,10 +15,15 @@ echo Connection is stable.
 :: Configuring Git
 echo.
 echo Configuring Git...
-git config --global core.autocrlf false
 git config core.autocrlf false
+git config core.ignorecase false
+git config core.fscache true
 git config diff.renameLimit 0
+git config status.renameLimit 0
 git config merge.renameLimit 0
+git config http.postBuffer 1048576000
+git config pack.threads 1
+git config index.threads 0
 
 :: Fetch GitHub
 echo.
